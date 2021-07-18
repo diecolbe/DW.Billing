3851f22d16b7dd9003bd7c024cd943ed0b7945f5
diecolbe <67524326+diecolbe@users.noreply.github.com>
2021-07-18 08:01:39 -0500

    Update Tbl_Cliente.sql
    Se actualiza archivo, con el srcript generado desde la BD

--- Tablas/Tablas/Tbl_Cliente.sql
+++ Tablas/Tablas/Tbl_Cliente.sql
@@ -1,12 +1,27 @@
-CREATE TABLE TBL_CLIENTE
+USE [DW.Billing]
+GO
+
+/****** Object:  Table [dbo].[Tbl_Cliente]    Script Date: 18/07/2021 8:00:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tbl_Cliente](
+	[Documento] [varchar](15) NOT NULL,
+	[Cod_tipo_documento] [int] NULL,
+	[Nombres] [varchar](30) NULL,
+	[Apellidos] [varchar](30) NULL,
+	[Direccion] [varchar](20) NULL,
+	[Telefono] [varchar](20) NULL,
+	[Email] [varchar](20) NULL,
+	[Fecha_Nacimiento] [datetime] NULL,
+ CONSTRAINT [PK_Tbl_Cliente] PRIMARY KEY CLUSTERED 
 (
-  DOCUMENTO VARCHAR(15),
-  COD_TIPO_DOCUMENTO INT,
-  NOMBRES VARCHAR(30),
-  APELLIDOS VARCHAR(30),
-  DIRECCION VARCHAR(20),
-  TELEFONO VARCHAR(20),
-  EMAIL   VARCHAR(20),
-  FECHA_NACIMIENTO DATETIME
-);
+	[Documento] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 
