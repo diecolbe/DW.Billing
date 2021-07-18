1ec95f351b1ca44ebb4c64da4293769931828ea0
diecolbe <67524326+diecolbe@users.noreply.github.com>
2021-07-18 08:07:16 -0500

    Update Tbl_Producto.sql
    Se actualiza archivo, con el srcript generado desde la BD

--- Tablas/Tablas/Tbl_Producto.sql
+++ Tablas/Tablas/Tbl_Producto.sql
@@ -1,8 +1,24 @@
-Create table TBL_PRODUCTO
+USE [DW.Billing]
+GO
+
+/****** Object:  Table [dbo].[Tbl_Producto]    Script Date: 18/07/2021 8:06:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tbl_Producto](
+	[Id_Producto] [int] IDENTITY(1,1) NOT NULL,
+	[Descripcion] [varchar](30) NULL,
+	[Precio] [int] NULL,
+	[Stock] [int] NULL,
+	[Fecha_ingreso] [datetime] NULL,
+ CONSTRAINT [PK_Tbl_Producto] PRIMARY KEY CLUSTERED 
 (
-  ID_PRODUCTO INT,
-  DESCRIPCION VARCHAR(30),
-  PRECIO  INT,
-  STOCK INT,
-  FECHA_INGRESO DATETIME
-);
+	[Id_Producto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
