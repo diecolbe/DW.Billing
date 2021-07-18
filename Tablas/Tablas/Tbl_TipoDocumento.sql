78b0f7df0092b22a25f24b2c4407b3d7d39161e4
diecolbe <67524326+diecolbe@users.noreply.github.com>
2021-07-18 08:10:59 -0500

    Update Tbl_TipoDocumento.sql
    Se actualiza archivo, con el srcript generado desde la BD

--- Tablas/Tablas/Tbl_TipoDocumento.sql
+++ Tablas/Tablas/Tbl_TipoDocumento.sql
@@ -1,5 +1,19 @@
-CREATE TABLE TBL_TIPODOCUMENTO
+USE [DW.Billing]
+GO
+
+/****** Object:  Table [dbo].[Tbl_TipoDocumento]    Script Date: 18/07/2021 8:09:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tbl_TipoDocumento](
+	[Id_Tipo_documento] [int] NOT NULL,
+	[Descripcion] [varchar](30) NULL,
+ CONSTRAINT [PK_Tbl_TipoDocumento] PRIMARY KEY CLUSTERED 
 (
-  ID_TIPO_DOCUMENTO INT,
-  DESCRIPCION VARCHAR(30)
-);
+	[Id_Tipo_documento] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
