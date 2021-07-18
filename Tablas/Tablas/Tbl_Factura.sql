fb2935b897d5326b3b96ea583becddd15f192d7b
diecolbe <67524326+diecolbe@users.noreply.github.com>
2021-07-18 07:57:44 -0500

    Update Tbl_Factura.sql
    Se modifica archivo con el script exportado desde la BD

--- Tablas/Tablas/Tbl_Factura.sql
+++ Tablas/Tablas/Tbl_Factura.sql
@@ -1,10 +1,25 @@
-CREATE TABLE TBL_FACTURA
+USE [DW.Billing]
+GO
+
+/****** Object:  Table [dbo].[Tbl_Factura]    Script Date: 18/07/2021 7:55:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tbl_Factura](
+	[Num_Factura] [varchar](20) NOT NULL,
+	[Codigo_cliente] [varchar](15) NULL,
+	[Nombre_Empleado] [varchar](30) NULL,
+	[Fecha_Facturacion] [datetime] NULL,
+	[Total_Factura] [decimal](10, 0) NULL,
+	[IVA] [decimal](10, 0) NULL,
+	[Estado] [varchar](1) NULL,
+ CONSTRAINT [PK_Tbl_Factura] PRIMARY KEY CLUSTERED 
 (
-  NUM_FACTURA VARCHAR(20),
-  CODIGO_CLIENTE VARCHAR(15),
-  NOMBRE_EMPLEADO VARCHAR(30),
-  FECHA_FACTURACION DATETIME,
-  TOTAL_FACTURA DECIMAL(10,0),
-  IVA DECIMAL(10,0)
-);
+	[Num_Factura] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
