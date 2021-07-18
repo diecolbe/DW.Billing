b67fdb37ac77184a6e14e41c4d514322ad07f2a5
diecolbe <67524326+diecolbe@users.noreply.github.com>
2021-07-18 08:04:58 -0500

    Update Tbl_Detalle_Factura.sql
    Se actualiza archivo, con el srcript generado desde la BD

--- Tablas/Tablas/Tbl_Detalle_Factura.sql
+++ Tablas/Tablas/Tbl_Detalle_Factura.sql
@@ -1,7 +1,23 @@
-CREATE TABLE TBL_DETALLE_FACTURA
+USE [DW.Billing]
+GO
+
+/****** Object:  Table [dbo].[Tbl_Detalle_Factura]    Script Date: 18/07/2021 8:02:16 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tbl_Detalle_Factura](
+	[Cod_Factura] [varchar](20) NOT NULL,
+	[Codigo_producto] [int] NOT NULL,
+	[Cantidad] [int] NULL,
+	[Total] [decimal](10, 0) NULL,
+ CONSTRAINT [PK_Tbl_Detalle_Factura] PRIMARY KEY CLUSTERED 
 (
-  COD_FACTURA VARCHAR(20),
-  CODIGO_PRODUCTO INT,
-  CANTIDAD INT,  
-  TOTAL DECIMAL(10,0)
-);
+	[Cod_Factura] ASC,
+	[Codigo_producto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
