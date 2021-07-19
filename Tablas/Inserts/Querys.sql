2c242b676fa379d8847c7cbce4c69e6e7d9cc024
diecolbe <67524326+diecolbe@users.noreply.github.com>
2021-07-18 22:31:54 -0500

    Update Querys.sql

--- Tablas/Inserts/Querys.sql
+++ Tablas/Inserts/Querys.sql
@@ -13,12 +13,12 @@ from Tbl_Producto;
 select descripcion, Precio 
 from Tbl_Producto;
 
---Obtener la lista de productos cuya existencia en el inventario haya llegado al mínimo permitido (5 unidades)
+--Obtener la lista de productos cuya existencia en el inventario haya llegado al mÃ­nimo permitido (5 unidades)
 select descripcion 
 from Tbl_Producto
 where stock<=5;
 
---Obtener una lista de clientes no mayores de 35 años que hayan realizado compras entre el 1 de febrero de 2000 y el 25 de mayo de 2000
+--Obtener una lista de clientes no mayores de 35 aÃ±os que hayan realizado compras entre el 1 de febrero de 2000 y el 25 de mayo de 2000
 select b.Nombres from 
 (select ( cli.Nombres+' '+cli.Apellidos)Nombres,
         (year(CURRENT_TIMESTAMP) - year(cli.Fecha_Nacimiento)) edad,
@@ -29,7 +29,7 @@ where fact.Codigo_cliente=cli. Documento) As B
 where b.edad<=35
 And Fecha_Facturacion BETWEEN '2021-01-28' AND '2021-07-17';
 
---Obtener el valor total vendido por cada producto en el año 2000
+--Obtener el valor total vendido por cada producto en el aÃ±o 2000
 Select pro.descripcion , sum(det.Total) total
 From Tbl_Factura fact
     JOIN Tbl_Detalle_Factura det ON fact.Num_Factura=det.Cod_Factura
@@ -37,12 +37,13 @@ From Tbl_Factura fact
 where Fecha_Facturacion BETWEEN '2021-01-01' AND '2021-07-16'
 Group by  det.Total,pro.descripcion;
 
---Obtener la última fecha de compra de un cliente y según su frecuencia de compra estimar en qué fecha podría volver a comprar.
+--Obtener la Ãºltima fecha de compra de un cliente y segÃºn su frecuencia de compra estimar en quÃ© fecha podrÃ­a volver a comprar.
 
 Select cli.Nombres+' '+cli.apellidos, 
-       Max(fecha_facturacion) ,
-       COUNT(fecha_facturacion) Frecuncy
+       Max(fecha_facturacion) UltimaCompra,
+       COUNT(fecha_facturacion) Frecencia
 From Tbl_Factura fact
 JOIN Tbl_Cliente cli on fact.codigo_cliente =cli.documento
 Group by cli.nombres,cli.apellidos;
+
 
